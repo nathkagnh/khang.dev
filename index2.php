9e34823dfe8e5ca13f9bb694fe02cdb60718dd50
nathkagnh <nathkagnh@gmail.com>
2016-01-14 15:47:17 +0700

    update

--- index2.php
+++ index2.php
@@ -6,7 +6,7 @@ $oCache = new CacheAPC();
 if($oCache->enable)
 {
 	$cacheData = $oCache->getData('myTest');
-	echo 'Data from memory: <pre>'
+	echo 'Data from memory: <pre>';
 	print_r($cacheData);
 	echo '</pre>';
 	echo 'Click <a href="index3.php">here</a> to delete cache';
