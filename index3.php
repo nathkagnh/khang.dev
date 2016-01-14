887db1685d0f975f005157eb433f847d0db1b1ad
nathkagnh <nathkagnh@gmail.com>
2016-01-14 15:42:40 +0700

    update

--- index3.php
+++ index3.php
@@ -5,10 +5,11 @@ $oCache = new CacheAPC();
 
 if($oCache->enable)
 {
+	$oCache->delData('myTest');
+
 	$cacheData = $oCache->getData('myTest');
+
 	echo 'Data from memory: <pre>'
 	print_r($cacheData);
 	echo '</pre>';
-	echo 'Click <a href="index3.php">here</a> to delete cache';
 }
-else echo 'APC not installed';
