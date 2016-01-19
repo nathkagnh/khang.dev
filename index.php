561294f86312a05cca984525072caabca9c50eff
nathkagnh <nathkagnh@gmail.com>
2016-01-19 15:08:30 +0700

    update

--- index.php
+++ index.php
@@ -4,7 +4,5 @@
 <?php
    //Connecting to Redis server on localhost
    $redis = new Redis();
-   $redis->connect('127.0.0.1', 6379);
-   echo "Connection to server sucessfully";
-   //check whether server is running or not
+   $redis->connect('172.30.113.179', 6379);
    echo "Server is running: "+ $redis->ping();
