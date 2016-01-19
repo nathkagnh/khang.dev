e33e8e0ad681da950001ee11da7dc6f1092e6a8a
nathkagnh <nathkagnh@gmail.com>
2016-01-19 15:05:21 +0700

    update

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <a href="phpinfo.php">Php info</a></br>
 <a href="apc/index.php">Apc</a></br>
+
+<?php
+   //Connecting to Redis server on localhost
+   $redis = new Redis();
+   $redis->connect('127.0.0.1', 6379);
+   echo "Connection to server sucessfully";
+   //check whether server is running or not
+   echo "Server is running: "+ $redis->ping();
