6590a593972125571b674adae1eb55be694d9dd4
nathkagnh <nathkagnh@gmail.com>
2016-01-20 14:48:16 +0700

    update

--- redis.php
+++ redis.php
@@ -11,6 +11,11 @@ try {
 $redis->set('email', 'nathkangh@gmail.com');
 $redis->set('name', 'Lê Đỗ Nhật Khang');
 
+$sdt = '0974269381';
+$address = 'abcxyz';
+
+$redis->set('array', array('sdt' => $sdt, 'address' => $address));
+
 echo 'successful';
 ?>
 </br><a href="redis2.php">click to see data from redis</a>
--- redis2.php
+++ redis2.php
@@ -5,3 +5,7 @@ $redis->connect('172.30.113.179', 6379);
 
 echo '<p>Name: ' . $redis->get('name') . '</p></br>';
 echo '<p>Email: ' . $redis->get('email') . '</p></br>';
+
+echo '<pre>';
+printf($redis->get('array'));
+echo '</pre>';
